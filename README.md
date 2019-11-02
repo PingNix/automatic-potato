70ae101466cba4afc06d8c83907c706016722f92
Glitch (capable-pruner) <none>
2019-11-02 14:19:46 +0000

    🏣🎪 Checkpoint ./README.md:5665662/103 ./bot.js:5665662/17

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## GELİŞMİŞ ALTYAPI BOTU - https://codemarefi.blogspot.com - TAFINDAN PAYLAŞILMIŞTIR
 GELİŞMİŞ ALTYAPIYA SAHİP BU BOT CODEMAREFİ - [Sadista] - tarafından paylaşılmıştır.
 
-herhangi
+herhangi bir aldıgınız hatada sorunda sitemizin yorumlar kısmında belirtirseniz yardımcı oluruz..
--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ const log = message => {
 };
 
 client.on("ready", () => {
-  client.user.setGame(`!!yardım | !!davet | asistanbott.glitch.me`, "https://www.twitch.tv/dexter036d");
+  client.user.setGame(`!!yardım | !!davet | asistanbott.glitch.me`, "https://www.twitch.tv/ddfsd7f01");
         }, 
   console.log("Bağlandım!")
 );
