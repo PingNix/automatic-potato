c0c2332eb470378b2d2c5946ef0ee88933d3590b
Glitch (capable-pruner) <none>
2020-01-04 10:39:15 +0000

    🖇🚘 Checkpoint ./komutlar/kayıtkız.js:8779579/2085

--- komutlar/kayıtkız.js
+++ komutlar/kayıtkız.js
@@ -1,24 +1,37 @@
 const Discord = require('discord.js');
+const db = require('quick.db');
 
-exports.run = async (client, message, args) => {
-  const ayarlar = require('../ayarlar.json');
-  let ıd = ayarlar.yetkili
-  if(!message.member.roles.has(ıd)) return message.reply(`Malesef Yetkin Yetersiz Dostum!`)
-  
+exports.run = (client, message, args) => {
+
+const kız = message.guild.roles.find(r => r.id === "ERKEK ROL İD")
+
+if (!message.member.hasPermission("MANAGES_ROLES")) return message.reply(`Bu komutu kullanabilmek için yeterli yetkin yok!!`);
+ let member = message.mentions.users.first() || client.users.get(args.join(' '))
+      if(!member) return message.channel.send(new Discord.RichEmbed().setColor('BLUE').setDescription("Bir kullanıcı girin."))
+      
+          const kayıt = message.guild.member(member)
+    let isim = args[1]
+let yas = args[2]
 let kişi = message.mentions.members.first();
-let kızrol = '662954133501771789'
-if(!kişi) return message.reply("Bir Kişi Eitketle!")
-  kişi.addRole(kızrol)
-  message.reply(`${kişi} Kullanıcısını Kız Olarak Kaydettim!`)
- 
-}
-exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ['kayıtkız'],
-    permLevel: 0
-}
+ if(!kişi) return 
 
-exports.help = {
-    name: 'kız'
-}    
+      if (!isim) return message.channel.send(new Discord.RichEmbed().setColor('BLUE').setDescription("bir isim girin."))
+      if (!yas) return message.channel.send(new Discord.RichEmbed().setColor('BLUE').setDescription("bir yaş girin."))
+     kayıt.addRole(kız)
+      kayıt.setNickname(`${isim} ${yas}`)
+      return message.channel.sendEmbed(new Discord.RichEmbed().setColor('BLUE').setDescription(`${kişi} Başarıyla  kaydettim.`)
+      )
+}
+      
+      exports.conf = {
+      enabled: true,
+      guildOnly: false,
+      aliases: ['kız-kayıt','kız','kaydet'],
+      permLevel: 2,
+      }
+      
+      exports.help = {
+      name: 'kız',
+      description: 'Erkek Kaydı yapar.',
+      usage: 'kız'
+      }
