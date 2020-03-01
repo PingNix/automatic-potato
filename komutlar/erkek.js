e64e198c0725931262744b58d5a4561260d7b826
Glitch (resonant-reading) <none>
2020-03-01 20:13:59 +0000

    🙌🌈 Checkpoint ./komutlar/erkek.js:5558253/3015:11589155/165

--- komutlar/erkek.js
+++ komutlar/erkek.js
@@ -3,16 +3,16 @@ const db = require("quick.db")
 
 exports.run = async (client, message, args) => {
 
-if(!message.member.roles.has("678362831502180355")) return message.channel.send(`Bu komutu kullanabilmek için \`♨️ | Commanders\` yetkisine sahip olmasınız.`);
+if(!message.member.roles.has("673706812800892932")) return message.channel.send(`Bu komutu kullanabilmek için \`🔦❜ Registrar.†\` yetkisine sahip olmasınız.`);
   let kullanıcı = message.mentions.users.first()
   if (!kullanıcı) return message.channel.send('Kullanıcıyı etiketlemeyi unuttun kanka.')
   let rol = message.mentions.roles.first()
   let member = message.guild.member(kullanıcı)
   member.addRole('673707723078107137')
-  member.removeRole('678362854579109909')
+  member.removeRole('673707957858467881')
     let embed = new Discord.RichEmbed()
   .setColor('RED')
-  .setDescription(` ${kullanıcı} **üyesine**  **rolü verildi!**`)
+  .setDescription(` ${kullanıcı} **üyesine** <@&673707723078107137> **rolü verildi!**`)
   .setFooter(`Komutu kullanan yetkili : ${message.author.username}`) 
   return message.channel.send(embed)
 };
